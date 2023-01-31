2b344dfac8753ff948bbac4b909e8738b0d2cdcc
wante98 <wante98@aol.com>
2023-01-31 23:44:46 +0800

    加入專案檔案。

---
+++ eCMS/Controllers/HomeApiController.cs
@@ -0,0 +1,81 @@
+﻿using eCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace eCMS.Controllers
+{
+    public class HomeApiController : HomeController
+    {
+
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    var employees = db.Employee.ToList();
+        //    return View(employees);
+        //}
+
+
+
+        [HttpPost]
+        public ActionResult Create(Employees employee)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Error = false;
+                var temp = db.Employees.Where(m => m.EmployeeID == employee.EmployeeID)
+                    .FirstOrDefault();
+                if (temp != null)
+                {
+                    ViewBag.Error = true;
+                    return View(employee);
+                }
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //public ActionResult Edit(string fEmpId)
+        //{
+        //    var employee = db.Employees
+        //        .Where(m => m.EmployeeID == fEmpId).FirstOrDefault();
+        //    return View(employee);
+        //}
+
+        //[HttpPost]
+        //public ActionResult Edit(Employees employee)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var temp = db.Employees
+        //            .Where(m => m.EmployeeID == employee.EmployeeID)
+        //            .FirstOrDefault();
+        //        temp.fName = employee.fName;
+        //        temp.fSalary = employee.fSalary;
+        //        temp.fMail = employee.fMail;
+        //        temp.fGender = employee.fGender;
+        //        temp.fEmploymentDate = employee.fEmploymentDate;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(employee);
+        //}
+
+        public ActionResult Delete(int EmployeeID)
+        {
+            var employee = db.Employees
+                .Where(m => m.EmployeeID == EmployeeID).FirstOrDefault();
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ eCMS/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using eCMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Employee()
+        {
+            var employees = db.Employees
+                //.Select(e => new { e.EmployeeID, e.FirstName, e.HireDate.ToString("dd/MM/yyyy") })
+                .ToList();
+            ViewBag.Message = "Employee List";
+
+            return View(employees);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var employee = db.Employees
+                .Where(m => m.EmployeeID == id).FirstOrDefault();
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Employee");
+        }
+    }
+}
